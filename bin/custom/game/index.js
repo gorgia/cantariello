2cabcc0c86531ae4aea5fc34aca0b83b5ffb47c3
Andrea <gorgia@users.noreply.github.com>
2018-08-17 16:20:58 +0200

    rain

--- bin/custom/game/index.js
+++ bin/custom/game/index.js
@@ -1,11 +1,9 @@
-const axios = require('axios')
 const myCache = require('../cache').myCache
 
 module.exports = class Game {
   constructor (props) {
     this.io = props.io
-    this.playerList = props.playerList
-    this.playerFirstChoiceList = props.playerFirstChoiceList
+    this.playerList = new Map()
     this.playerChoice = new Map()
   }
 
@@ -13,7 +11,9 @@ module.exports = class Game {
     let self = this
     this.io.on('connection', (socket) => {
       console.log(`Client connected [id=${socket.id}], number of clients: ${this.clientsConnected()}`)
-      console.log(`retrieved user connected\n${socket.handshake.query.user}`)
+      let user = socket.handshake.query.user
+      console.log(`retrieved user connected\n${user}`)
+      this.playerList.set(socket.id, user)
       socket.on('SEND_MESSAGE', function (data) {
         console.log('SEND_MESSAGE:' + data)
         self.io.emit('MESSAGE', data)
@@ -24,7 +24,15 @@ module.exports = class Game {
         self.playerChoice.set(data.user._id, data.playerFirstChoice)
         self.io.emit('FIRST_CHOICE_RECEIVED', data)
       })
+      socket.on('PLAYER_TURN_CHOICE', function (data) {
+        console.log(`PLAYER_TURN_CHOICE_RECEIVED`, data)
+        self.io.emit(`DISABLE_BUTTON`)
+      })
     })
+  }
+
+  addPlayerToPlayerList (socketid, user) {
+    this.playerList.set(socketid, user)
   }
 
   clientsConnected () {
@@ -32,5 +40,34 @@ module.exports = class Game {
     let clientsConnected = Object.keys(srvSockets).length
     myCache.set(`clientsConnected`, clientsConnected)
     return clientsConnected
+  }
+
+  processPlayerChoice (player, choice) {
+    let usersEliminated = this.eliminatePlayersByChoice(choice)
+    this.io.emit('PLAYERS_ELIMINATED', usersEliminated)
+  }
+
+  eliminatePlayersByChoice (choice) {
+    let usersEliminated = []
+    let self = this
+      this.playerChoice.forEach((key, value) => {
+        if (value === choice) {
+          let userEliminated = self.eliminatePlayerByUserId(key)
+          if (userEliminated) usersEliminated.push(userEliminated)
+          this.playerChoice.delete(key)
+        }
+      })
+    return usersEliminated
+  }
+
+  eliminatePlayerByUserId (userId) {
+    let userEliminated = null
+    this.playerList.forEach(function (key, value) {
+      if (value._id === userId) {
+        userEliminated = value
+        this.playerList.delete(key)
+      }
+    })
+    return userEliminated
   }
 }
